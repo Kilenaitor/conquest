95d0e4b5f9b03cf6adce2123b12b85b820163236
Kyle Minshall <kyleminshall@gmail.com>
2016-02-02 19:57:27 -0800

    first commit

---
+++ game.js
@@ -0,0 +1,16 @@
+$(function() {
+  var hexagonGrid = new HexagonGrid("main_game", 50);
+  hexagonGrid.drawHexGrid(7, 7, 200, 50, true);
+  var color1 = "RGBA(16, 193, 72, 0.3)";
+  var color2 = "RGBA(172, 11, 0, 0.3)";
+  hexagonGrid.drawCapitals(color1, color2);
+  hexagonGrid.getAdjacentCells(1,1);
+});
+
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
